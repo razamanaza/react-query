a4da3f20f9b3f218e5e8f6c30b7b1faa5db365f2
Aleksandr Gordeev <razamanaza@gmail.com>
2024-02-24 18:36:19 +1300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,21 @@ function wait(duration) {
 }
 
 function App() {
+  const queryClient = useQueryClient();
+
   const postQuery = useQuery({
     queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: async (title) => {
+      await wait(1000);
+      return POSTS.push({ id: crypto.randomUUID(), title });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
   });
 
   if (postQuery.isLoading) return <h1>Loading...</h1>;
@@ -23,6 +35,12 @@ function App() {
       {postQuery.data.map((post) => (
         <div key={post.id}>{post.title}</div>
       ))}
+      <button
+        disabled={newPostMutation.isPending}
+        onClick={() => newPostMutation.mutate("New Post")}
+      >
+        Add post
+      </button>
     </div>
   );
 }
